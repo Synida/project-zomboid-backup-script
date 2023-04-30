feb3682deb7d125c171ea59fcecb71f51cdf9f63
Synida <synida@protonmail.com>
2023-04-30 20:12:49 +0300

    v2.0.0
    + Doesn't save the whole environment, only the player db, thumbnail and reanimation files
    + Due to smaller save folders, synchronization doesn't really take time comparing to the old version
    + Default script doesn't require manual configuration in most cases - when the game is on C drive
    + saving frequency is increased from 1 per 6 mins to 1 per 2 mins
    + Script checks for all whitelisted game types and attempts to save any game, if the player database is changed in them. If the player database is not changed no save file will be created
    + script to handle path names containing whitespaces
    + detected empty game mode folders wont crash the script, wont create empty save folders
    + updated readme to v2.0.0

--- Project Zomboid with auto backup.bat
+++ Project Zomboid with auto backup.bat
@@ -6,22 +6,22 @@
     :: Project Zomboid folder location - dont leave backslash(\) at the end
     set gamePath=C:\Program Files (x86)\Steam\steamapps\common\ProjectZomboid
 
-    :: Project Zomboid executable - use 32/64 version according to your OS
+    :: Project Zomboid executable name - use 32/64 version according to your OS
     set exeName=ProjectZomboid64.exe
 
-    :: Project Zomboid save file path - you need to define Sandbox or Survival at the end, set <username> according to your environment - dont leave backslash(\) at the end - replace <osUserFolderName> with your OS user account foldername
-    set savePath=C:\Users\<osUserFolderName>\Zomboid\Saves\Sandbox
+    :: Project Zomboid default save file path
+    set savePath=%USERPROFILE%\Zomboid\Saves
 
-    :: World name that you would like to back up while playing Project Zomboid - replace <saveFolderName> parameter with your save folder name inside the save path folder e.g. 15-08-2022_11-58-14
-    set saveFolder=<saveFolderName>
+    :: PZ game modes - or more like the save folder names that belogs to them - whitelist
+    set gameModes=Sandbox Survivor Apocalypse Builder Kingsmouth "A Really CD DA" "A Storm is Coming" "Studio" "The Descending Fog" "Winter is Coming" "You Have One Day"
 
-    :: Backup folder location where the backups will be saved - dont leave backslash(\) at the end - replace <osUserFolderName> with your OS user account foldername
-    set backupFolder=C:\Users\<osUserFolderName>\Zomboid\Saves\Backup
+    :: Backup folder location where the backups will be saved
+    set backupFolder=%USERPROFILE%\Zomboid\Saves\Backup
 
-    :: Backup frequency in seconds - if u love your ssd, don't set it to something too small
-    set backupFrequency=300
+    :: Backup frequency in seconds - basically the save script execution frequency and not the game's save frequency
+    set backupFrequency=120
 
-    :: This many of the last backups will be kept, the rest will be deleted over time to save space
+    :: This many of the last backups will be kept, the rest will be deleted over time to not trash your backup folders
     set keepBackups=15
 
     ::::::::::::::::::::::::::::
@@ -31,21 +31,21 @@
     if not exist "%gamePath%\" (
         echo gamePath folder doesnt exists or incorrect, check the config
         pause
+        exit
     )
     if not exist "%gamePath%\%exeName%" (
         echo game exe can not be found with this name, check the config
         pause
+        exit
     )
     if not exist "%savePath%" (
         echo save path can not be found, check the config
         pause
-    )
-    if not exist "%savePath%\%saveFolder%" (
-        echo save folder doesnt exist/was removed/incorrect, check the config
-        pause
+        exit
     )
 
-    echo I hope you configured your setup properly
+    echo I hope you configured your setup properly.
+    echo Base path validation passed.
 
     ::::::::::::::::::::::::::::::
     :: Starting Project Zomboid ::
@@ -62,27 +62,65 @@
 
     :: Create backup folder if doesn't exist
     if not exist "%backupFolder%" mkdir "%backupFolder%"
-    if not exist "%backupFolder%\%saveFolder%" mkdir "%backupFolder%\%saveFolder%"
 
-    cd "%backupFolder%\%saveFolder%"
-    set saveLocation=%savePath%\%saveFolder%
+    setlocal EnableDelayedExpansion
 
     :: Infinite loop running to back up the world stages in every %backupFrequency% seconds
     :loop
         :: Wait until the next scheduled backup
         timeout /t %backupFrequency% /nobreak > nul
 
-        set subDate=%date:/=_%
-        set subTime=%time::=_%
-        set timestamp=%subDate: =_%_%subTime:.=_%
-        echo %timestamp%
+        for %%a in (%gameModes%) do (
+            if exist "%savePath%\%%~a" (
+                cd "%savePath%\%%~a"
 
-        echo Saving the world.
-        set backupLocation=%backupFolder%\%saveFolder%\%timestamp%
+                set count=0
+                for /f %%b in ('dir /b /ad "%savePath%\%%~a"') do (
+                    set /A count+=1
+                )
 
-        robocopy "%saveLocation%" "%backupLocation%" /E
+                if not !count! == 0 (
+                    set n=0
+                    :: Checking for game directories
+                    for /d %%d in (*.*) do (
+                        set games[!n!]=%%d
+                        set /A n+=1
+                    )
 
-        :: Delete all old saves except last %keepBackups% - based on file name, so don't change them
-        for /f "skip=%keepBackups% eol=: delims=" %%F in ('dir /b /o-n /AD') do @rmdir /q /s "%%F"
+                    if not !n! == 0 (
+                        set /A n-=1
+                    )
+
+                    for /L %%i in (0,1,!n!) do (
+                        set saveFolder=!games[%%i]!
+                        cd "%backupFolder%\%saveFolder%"
+
+                        :: Create save folder if it doesn't exist
+                        if not exist "%backupFolder%\!saveFolder!" mkdir "%backupFolder%\!saveFolder!"
+
+                        :: get the last modification time of the game's player database
+                        for /f %%j in ('PowerShell -NoProfile -NoLogo -Command "(Get-Item '%savePath%\%%~a\!saveFolder!\players.db').LastWriteTime | Get-Date -Format yyyy-MMM-dd-HH-mm-ss"') do set timestamp=%%j
+
+                        if not exist "%backupFolder%\!saveFolder!\!timestamp!" (
+                            echo Saving the world for the %%~a game: !saveFolder!
+                            mkdir "%backupFolder%\!saveFolder!\!timestamp!"
+                            robocopy "%savePath%\%%~a\!saveFolder!" "%backupFolder%\!saveFolder!\!timestamp!" players.db reanimated.bin thumb.png
+                        ) else (
+                            echo No changes has been detected in the %%~a game: !saveFolder!
+                        )
+
+                        :: Delete all old saves except last %keepBackups% - based on folder naming, not modification dates
+                        for /f "skip=%keepBackups% eol=: delims=" %%F in ('dir /b /o-n /AD') do @rmdir /q /s "%%F"
+                    )
+                    :continue
+                    echo\
+                ) else (
+                    echo The %%~a folder is empty. Skipping.
+                )
+            ) else (
+                echo No %%~a game found. Skipping.
+            )
+        )
     goto:loop
+    endlocal
 exit
--- README.md
+++ README.md
@@ -1,21 +1,44 @@
-# Project Zomboid backup script
+# Project Zomboid game autosave patch script
 
-This is a configurable basic script to help you to generate automated backups from the game save.
-It will start the game upon execution - if you are logged into steam - then start producing copies of your selected game world with the configured frequency.
-Mind that it can't save the game more frequently then as the game saves the world status itself.
-Since the game save folder can be quite big, producing the copies can take quite the time as well.
-The script will also limit the copies being made to the specified amount, and delete the older backup folder - over the backup limit u wish to keep.
+This is a configurable windows batch script that automatically saves any singleplayer games of the whitelisted game modes you start.
 
-The backup frequency actually defines the time between two backup from the previous backup's completion to the start of the new backup.
-Meaning that if creating a backup takes 5 minutes and you set your backup frequency to 15 minutes, then the next backup will be created 20 minutes later.
+https://github.com/Synida/project-zomboid-backup-script
 
-To restore a backup you need to copy the backup folder back to the save folder - and rename there if you wish.
-Do not rename the folder names in the backup folder, because the backup limit deletion is based on the folder names.
+### Requirements
+Basically nothing. The script uses PowerShell at some point, which is by default part of most Windows environments.
+You migth experience difficulties with your anti-virus or win UAC upon downloading it, but the script is really open source with mostly commented lines,
+so you can check that there aren't any nasty things in the code itself.
 
-Mind that UAC(windows' user account control) prevent you from executing the script, if you download it from the internet.
+### Installation
+1. Download the script - from Releases section(the ZIP file - last version)
+2. Unzip the .bat file
+3. Run the .bat file to start the game and the autosave at once
+4. Enjoy.
 
-### Known issue
-- If the game save happens in the same time with the backup generation, it is possible that some of the files will have the older version stored
+You can also open up your favorite text editor like notepad, notepad++, sublime, whatever - and just copy the code from the bat file that you can find on GitHub.
 
-### Future improvement possibility
-The saving time can be greatly reduced if the backups are made only from the changed files since the last backup.
+### How does this work?
+If you execute the .bat file a command line terminal will show up where the script will be running.
+The script will check for PZ installation and save directories in your system.
+If all of them are fine, and Steam is running, then it will start the game for you.
+As you play the script will automatically detect if any of the games that you played before has any change in them.
+If there is, it will create a save file, that you can use to reload your character to a previous part of his/her life.
+This means that both wounds and the position of your character will revert to a previous state.
+If the zombies eat your brain and the game would try to force you to start everything over, you can reload your game and continue it with all the experiences,
+tools, equipment, wounds, that you have in that previous state of your life,
+but you will be able to find your dead or zombified body at the last position where you died as well.
+
+The script will automatically save every 2 minutes, which you can change in the script.
+The game will store up to 15 save folders, and delete the older ones as it creates newer ones.
+This can be changed in the script with a text editor as well to your liking.
+
+### How to reload the save?
+PZ game save location by default is: `%USERPROFILE%\Zomboid\Saves`.
+You can access this easily by pressing `WIN + R` buttons at once, copy it there and pressing enter.
+Here you can find your games and a folder named `Backup`. The `Backup` folder is only generated if you played with the script before.
+Here you can find save folders for each game you played before, and each of these save folders will contain sub folders with date time names.
+You find the game you want to reload here, and copy the content - `players.db`, `reanimated.bin`, `thumb.png` - back to the game's main save folder, overwriting the current files in the process.
+
+### Known issues
+- `!` character in the game name is not to the script's liking
+- if you installed the Steam to an other partition the C, then you will have to change the driver letter in the script configuration section manually.
